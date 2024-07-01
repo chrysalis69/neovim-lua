ef8ee48065605303e3e23bfa0f33ded3982bb192
Stefan Coetzee <chrysalis69@gmail.com>
2024-07-01 09:36:08 +0200

    Add rainbow-delimiter

---
+++ lua/chrysalis/lazy/rainbow_delimiter.lua
@@ -0,0 +1,26 @@
+return {
+    "hiphish/rainbow-delimiters.nvim",
+    config = function()
+	local rainbow_delimiters = require("rainbow-delimiters")
+        require("rainbow-delimiters.setup").setup {
+		strategy = {
+	            [''] = rainbow_delimiters.strategy['global'],
+	            commonlisp = rainbow_delimiters.strategy['local'],
+	        },
+	        query = {
+	            [''] = 'rainbow-delimiters',
+	            latex = 'rainbow-blocks',
+	        },
+	        highlight = {
+		            'RainbowDelimiterRed',
+		            'RainbowDelimiterYellow',
+		            'RainbowDelimiterBlue',
+		            'RainbowDelimiterOrange',
+		            'RainbowDelimiterGreen',
+		            'RainbowDelimiterViolet',
+		            'RainbowDelimiterCyan',
+		        },
+	        blacklist = {'c', 'cpp'},
+	}
+    end
+}
